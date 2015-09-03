8e21889b3f39fe6085a04491c5bbb796fa8b0922
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2015-09-03 14:59:16 +0200

    2015-09-03 gallery project config MySql db to do

--- 08_oop/oop_practice.php
+++ 08_oop/oop_practice.php
@@ -155,8 +155,8 @@
         static $foo;
     }
 
-    class Two extends One {};
-    class Three extends One {};
+    class Two extends One {}
+    class Three extends One {}
 
     One::$foo = 1;
     Two::$foo = 2;
@@ -173,6 +173,137 @@
 
     // scope resolution operator, Paamayim Nekudotayim, One Doubled Dot Doubled
     // dwukropek po hebrajsku
+
+    class A {
+        static $a = 1;
+
+        static function modified_a() {
+            // self wskazuje na klasę ($this na instancję, i to dla niestatycznych pól)
+            // to samo co
+            //return A::$a + 10;
+            return self::$a + 10;
+        }
+
+        function hello() {
+            return "hello";
+        }
+    }
+
+    class B extends A {
+        static function attr_test() {
+            // A::$a
+            // B::$a
+            // też będzie działać, jest dziedziczenie, a ponadto
+            // statyczność sprawia, że referencja jest taka sama
+            return parent::$a;
+        }
+
+        static function method_test() {
+            return parent::modified_a();
+        }
+
+        function instance_test() {
+            return $this->hello();
+            // parent:: będzie działał z dynamiczną metodą, nie z dyn. atrybutem
+            //return parent::hello();
+        }
+
+        function hello() {
+            return parent::hello();
+        }
+    }
+
+    // w przypadku dynamicznych pól również można używać parent
+    // tylko z metodami
+
+    echo "a: " . B::$a . "<br />";
+    echo "modified a: " . B::modified_a() . "<br />";
+
+    echo "attr_test: " . B::attr_test() . "<br />";
+    echo "method_test: " . B::method_test() . "<br />";
+
+    $newB = new B();
+    echo "this->, wywołanie z B: " . $newB->instance_test() . "<br />";
+    echo "parent::, wywołanie z A: " . $newB->hello() . "<br />";
+
+    class Furniture {
+        function __construct() {
+
+        }
+    }
+
+    class Table extends Furniture {
+        public $legs;
+        static public $total_tables = 0;
+
+        // można przekazywać argumenty
+        function __construct($leg_count = 4) {
+            $this->legs = $leg_count;
+            Table::$total_tables += 1;
+            // można wywołać konstruktor rodzica
+            parent::__construct();
+        }
+
+        // destruktor nie jest konieczny
+        // klasa istnieje na czas request, potem PHP ją usuwa
+        // przydaje się przy ręcznym usuwaniu instancji
+        function __destruct() {
+            Table::$total_tables -= 1;
+        }
+    }
+
+    $table = new Table();
+    echo "table: " . $table->legs . "<br />";
+    echo "total tables: " . $table::$total_tables . "<br />";
+    echo "total tables stat: " . Table::$total_tables . "<br />";
+    // dla metody działało
+    //echo "total tables: " . $table->total_tables . "<br />";
+
+    // przekazujemy argument do konstruktora
+    $t2 = new Table(6);
+    echo "legs: " . $t2->legs . "<br />";
+    echo "total tables stat: " . Table::$total_tables . "<br />";
+
+    class Beverage {
+        public $name;
+
+        function __construct() {
+            echo "new beverage __construct" . "<br />";
+        }
+
+        // konstruktor dla sklonowanego obiektu
+        function __clone() {
+            echo "cloned beverage __clone" . "<br />";
+        }
+    }
+
+    $bev1 = new Beverage();
+    $bev1->name = "coffee";
+    $bev2 = $bev1;
+    $bev2->name = "tea";
+    echo "bev1: " . $bev1->name . "<br />";
+
+    $bev3 = clone $bev1;
+    $bev3->name = "juice";
+    echo "bev3: " . $bev3->name . "<br />";
+    echo "bev1: " . $bev1->name . "<br />";
+
+    $b1 = new Beverage();
+    $b2 = $b1;
+    $b3 = clone $b1;
+    $b4 = clone $b3;
+    $b4->name = "milk";
+    $b5 = new Beverage();
+
+    echo ($b1 == $b2 ? 'true' : 'false') . "<br />"; // true
+    echo ($b1 == $b3 ? 'true' : 'false') . "<br />"; // true
+    echo ($b1 == $b4 ? 'true' : 'false') . "<br />"; // zmieniony klon, false
+    echo ($b1 == $b5 ? 'true' : 'false') . "<br />"; // true
+    echo "<hr />";
+    echo ($b1 === $b2 ? 'true' : 'false') . "<br />"; // referencja, true
+    echo ($b1 === $b3 ? 'true' : 'false') . "<br />"; // false
+    echo ($b1 === $b4 ? 'true' : 'false') . "<br />"; // false
+    echo ($b1 === $b5 ? 'true' : 'false') . "<br />"; // flase
     ?>
 
     </body>
---
+++ 09_php_gallery_project/public/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>PHP Gallery</title>
+        <link href="stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+
+    <div id="header">
+
+    </div>
+
+    <div id="main">
+
+    </div>
+
+    <div id="footer">
+
+    </div>
+
+    </body>
+</html>
